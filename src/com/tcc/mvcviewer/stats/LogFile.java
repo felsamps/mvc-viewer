3c2c73a6c439fa5a007c21ee10b4cf0f09af836d
Felipe Sampaio <felsamps@agiledh.com>
2010-07-04 23:36:01 -0300

    LogFile class

---
+++ src/com/tcc/mvcviewer/stats/LogFile.java
@@ -0,0 +1,61 @@
+package com.tcc.mvcviewer.stats;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author felsamps
+ */
+public class LogFile {
+	private static FileWriter logFile;
+	private static BufferedWriter writer;
+	private static Integer views, frames, gop;
+	private static Integer numEntries;
+	
+	static {
+		//variables initialization
+		numEntries = 0;
+		try {
+			logFile = new FileWriter("/home/Tcc/mvc-viewer/data/output-files/logFile.log");
+			writer = new BufferedWriter(logFile);
+			writeHeader();
+		}
+		catch( IOException ex ) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void closeLogFile() {
+		try {
+			writer.close();
+		}
+		catch( IOException ex ) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void init(Integer views, Integer frames, Integer gop) {
+		LogFile.views = views;
+		LogFile.gop = gop;
+		LogFile.frames = frames;
+	}
+
+	public static void log(String str) {
+		try {
+			writer.write(str);
+		}
+		catch( IOException ex ) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void incNumEntries() {
+		numEntries ++;
+	}
+
+	private static void writeHeader() {
+		//TODO implement it!
+	}
+}
